64a5b61812cfcdd12ee51b1847554795e0cd9fe9
Alexis-Santana-Vega <santanavega.alexis@gmail.com>
2024-10-23 19:31:30 -0600

    CE Backend: Cambios por errores

--- CE.Chepeat.Application/Controllers/ApiController.cs
+++ CE.Chepeat.Application/Controllers/ApiController.cs
@@ -15,19 +15,22 @@ namespace CE.Chepeat.Application.Controllers
         private readonly IMapper _mapper;
         private readonly IConfiguration _configuration;
         private readonly IJwtService _jwtService;
+        private readonly IEmailService _emailService;
 
 
-        public ApiController(IUnitRepository unitRepository, IMapper mapper, IConfiguration configuration, IJwtService jwtService)
+        public ApiController(IUnitRepository unitRepository, IMapper mapper, IConfiguration configuration, IJwtService jwtService, IEmailService emailService)
         {
             _unitRepository = unitRepository;
             _mapper = mapper;
             _configuration = configuration;
             _jwtService = jwtService;
+            _emailService = emailService;
         }
 
         public IUserPresenter UserPresenter => new UserPresenter(_unitRepository, _mapper);    //
         public IAuthPresenter AuthPresenter => new AuthPresenter(_unitRepository, _mapper, _jwtService);
         public ISellerPresenter SellerPresenter => new SellerPresenter(_unitRepository);
         public IProductPresenter ProductPresenter => new ProductPresenter(_unitRepository);
+        public IEmailPresenter EmailPresenter => new EmailPresenter(_emailService, _unitRepository);
     }
 }
