61dcaa361c8c2e128de781fca7a3211130ee4901
Alexis-Santana-Vega <santanavega.alexis@gmail.com>
2024-11-09 08:41:29 -0600

    CE Backend: prueba de FileUpload

---
+++ CE.Chepeat.API/Controllers/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CE.Chepeat.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        public ImageController()
+        {
+        }
+
+        [HttpPost("{imageId}")]
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile image, string imageId)
+        {
+            return Ok();
+        }
+    }
+}
