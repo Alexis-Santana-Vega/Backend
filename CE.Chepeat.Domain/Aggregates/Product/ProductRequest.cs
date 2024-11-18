e9767317fd5618d692c6626a3f56872fca132929
Alexis-Santana-Vega <santanavega.alexis@gmail.com>
2024-11-18 14:36:17 -0600

    CE Backend: Implementación de Productos

--- CE.Chepeat.Domain/Aggregates/Product/ProductRequest.cs
+++ CE.Chepeat.Domain/Aggregates/Product/ProductRequest.cs
@@ -31,5 +31,8 @@ public class ProductRequest
     [StringLength(10, ErrorMessage = "Medida máxima 10 caracteres")]
     public string Measure { get; set; }
 
+    public string ImagenUrl { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "ID de Vendedor requerido")]
     public Guid IdSeller { get; set; }
 }
